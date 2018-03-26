9a6eb30aeb9823ad2d881c6605465e7117abbad4
Brian Azubuike <ba286@afsconnect1.njit.edu>
2018-03-25 22:19:57 -0400

    removed php link to input php filebecause it ran the entire file rather than just grabbing variables

--- entered.php
+++ entered.php
@@ -32,8 +32,6 @@
 	<body>
 		<a href="../JustForViews/index.html">I used my website on this link as a template for this sign up page</a>
 		<h2>Long Time No See,</br><?php echo "Sir"; ?></h2>
-		<?php include 'input.php';
-		echo '$first $last';?>
 	</body>
 
 	<footer>
